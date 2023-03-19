841707d9a1fde08f201f4f5cdc606b941b8a4784
serkanboyraci <serkanboyraci@gmail.com>
2023-03-19 21:22:05 +0300

    Initial Commit

---
+++ ThreadingAsyncImageSwiftUI/ThreadingAsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadingAsyncImageSwiftUIApp.swift
+//  ThreadingAsyncImageSwiftUI
+//
+//  Created by Ali serkan Boyracı  on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadingAsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
